6614d286416e200c39f286ea945cc59041773616
oriel2323 <oriel2510@gmail.com>
2026-06-08 13:06:30 +0300

    finished 1b

--- threeDecisionTree.py
+++ threeDecisionTree.py
@@ -20,3 +20,29 @@ y = iris.target
 #90% of the data for training and 10% for testing
 XTrain, XTest, yTrain, yTest = train_test_split(X,y,test_size = 0.1, random_state = 7, stratify = y) 
 
+# Create a decision tree classifier using entropy
+tree = DecisionTreeClassifier(criterion= "entropy" , random_state = 7)
+
+# Train the decision tree using the training data
+tree.fit(XTrain,yTrain)
+
+# Get the depth of the trained decision tree
+treeDepth = tree.get_depth()
+print("Tree depth: ", treeDepth)
+
+#Predict the labels for the test data
+yPred = tree.predict(XTest)
+
+# Calculate evaluation metrics
+#we use macro average because in this data set the catagories are not only 1 or 0 
+# thats why we need to do average. 
+accuracy = accuracy_score(yTest,yPred)
+precision = precision_score(yTest,yPred,average = "macro")
+recall = recall_score(yTest, yPred, average="macro")
+f1 = f1_score(yTest, yPred, average="macro")
+
+#print the values
+print("Accuracy: ", accuracy)
+print("Precision: ", precision)
+print("Recall: ", recall)
+print("F1-score: ", f1)
